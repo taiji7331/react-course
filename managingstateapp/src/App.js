04cead3d5b936a2c787cc5fe00f3c6383afebfa9
Kyle <taiji7331@gmail.com>
2022-09-11 15:16:15 +0900

    Added practice app for lifting state up

---
+++ managingstateapp/src/App.js
@@ -0,0 +1,21 @@
+import Fruits from "./components/Fruits";
+import FruitsCounter from "./components/FruitsCounter";
+import React from "react";
+
+function App() {
+  const [fruits] = React.useState([
+    {fruitName: 'apple', id: 1},
+    {fruitName: 'pear', id: 2},
+    {fruitName: 'plum', id: 3},
+  ]);
+
+  return (
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits}/>
+      <FruitsCounter fruits={fruits}/>
+    </div>
+  );
+}
+
+export default App;
